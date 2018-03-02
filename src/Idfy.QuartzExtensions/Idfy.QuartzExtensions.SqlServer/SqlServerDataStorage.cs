334b5425f89c0bcf6aa060e1ae12ce39b0cb4b25
Christoffer Slettan <christofferslettan@gmail.com>
2018-03-02 15:40:37 +0100

    Add SqlServerDataStorage

---
+++ src/Idfy.QuartzExtensions/Idfy.QuartzExtensions.SqlServer/SqlServerDataStorage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Idfy.QuartzExtensions.SqlServer
+{
+    internal class SqlServerDataStorage
+    {
+        private readonly string _connectionString;
+
+        internal SqlServerDataStorage(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        private async Task<IDbConnection> GetConnection()
+        {
+            SqlConnection connection = null;
+
+            try
+            {
+                connection = new SqlConnection(_connectionString);
+                await connection.OpenAsync();
+                return connection;
+            }
+            catch (Exception)
+            {
+                connection?.Dispose();
+                throw;
+            }
+        }
+    }
+}
